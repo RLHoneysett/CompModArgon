e42621de1280c551125f273cc940a4dcaa964562
Liger134 <60659153+Liger134@users.noreply.github.com>
2020-03-21 22:19:37 +0000

    Update particleManyBody.py
    The completed code from my end! 
    - fixed the RDF hopefully
    - simplified the MSD
    - program now prints msd and rdf to a file
    - did some re-ordering to make it more readable I hope
    Please make changes if you spot anything!

--- particleManyBody.py
+++ particleManyBody.py
@@ -5,11 +5,15 @@ the Lennard-Jones (LJ) Potential.
 Produces a trajectory file, with which the particles’trajectories can be visualised
 using a molecular visualisation program, VMD (Visual Molecular Dynamics).
 Writes the following to a file at regular time intervals:
-    1) the system’s total, kinetic and potential energies.
+    1) The system’s total, kinetic and potential energies.
        The file will be called energy.dat, and will have the format: <time> <KE> <PE> <Total Energy>
        The energies will also be plotted on a graph.
-    2) the particles’ mean square displacement (MSD).
-    3) the particles’ radial distribution function (RDF). This will also be visualised as a histogram.
+    2) The particles’ mean square displacement (MSD).
+       The file will be called msd.dat, and will have the format: <time> <MSD>
+       The MSD vs time will also be plotted on a graph.
+    3) The particles’ radial distribution function (RDF). 
+       The file will be called rsd.dat, and will have the format: <distance> <RDF value at this distance>
+       This will also be plotted on a graph.
 Note that the units used will be reduced units. These are explained below.
 
 The potential energy of the two particles in the LJ potential is:
@@ -17,12 +21,12 @@ The potential energy of the two particles in the LJ potential is:
 where:
     * r12 = abs(r2 - r1) (scalar)
     * r1 and r2 are the positions of the two particles (vectors)
-
+    
 The force on particle 1 (vector) is:
     F1(r1, r2) = 48[1/(r12^14)-1/(2*r12^8)](r1-r2)
 The force on particle 2 (vector) is:
     F2(r1, r2) = -F1(r1, r2)
-
+    
 The program will require an input file. This will contain the following, where relevant in
 reduced units:
     * number of particles
@@ -34,16 +38,12 @@ reduced units:
     * interval ( in terms of timesteps) at which the positions of the particles will be printed to the trajectory file
 The format will be:
     <no. particles> <no. steps> <timestep> <temp> <density> <LJ cut-off> <print interval>
-
+    
 The output trajectory file will have the format:
-
 <number of points to plot>
 <title line (the timestep number)>
 <particle label> <x coordinate> <y-coordinate> <z coordinate>
 The final line is repeated N times for N particles.
-
-The ouput files containing the equilibrium properties of the simulated material will have the format:
-    *** complete this later***
 
 Reduced units for length, energy, mass, temperature and time will be used.
 These (labelled with an asterisk) are given by:
@@ -53,7 +53,7 @@ and k_B is the Boltzmann constant.
 
 Author: Damaris Tan and Rachel Honeysett
 Student number: s1645055 and s1711116
-Version: 20/02/20
+Version: 21/03/20
 """
 
 import sys
@@ -89,7 +89,6 @@ def force_lj(particle1, particle2, box_size, lj_cutoff):
 
     return force
 
-
 def pot_energy_lj(particle1, particle2, box_size):
     """
     Method to return potential energy of two particles interacting via the LJ potential.
@@ -107,11 +106,9 @@ def pot_energy_lj(particle1, particle2, box_size):
 
     return potential
 
-
 def total_force(particle, particles, num_particles, lj_cutoff, box_size):
     """
     Method to compute the total force on one particle, due to all the other particles.
-
     :param particle: Particle3D instance - the particle in question
     :param particles: list of all the Particle3D instances in simulation box
     :param num_particles: the number of particles in the simulation box
@@ -131,58 +128,60 @@ def total_force(particle, particles, num_particles, lj_cutoff, box_size):
     return my_force
 
 #Mean Square Displacement function
-def MSD(particle1, initial_pos, box_size):
+def msd(particle1, initial_pos, box_size):
     """
     Method to find the average MSD over all particles.
     Uses the equation:
     MSD(t) = (1/N)*sigma(|ri(t) - ri0|^2)
-
     :param particle1: a Particle3D instance.
     :param initial_pos: the initial position of particle1
     :param box_size: length of the simulation box (a cube)
-
     :return: the sum of the MSDs for all particles, with each other.
     """
-    SD = (np.linalg.norm( np.mod((initial_pos - particle1.position)+ box_size/2, box_size) - box_size/2))**2  #finds SD
-
+    # Note: Use same method as image_closest_to_particle() function
+    # but this requires two Particle3D instances, whereas the initial positions will be stored as coordinates. 
+    # Hence the displacement is calculated explicitly here.
+    SD = (np.linalg.norm( np.mod((initial_pos - particle1.position)+ box_size/2, box_size) - box_size/2))**2  #finds SD      
+    
     return SD
-
 
 #collects data for radial distribution function
 
-def RDF_collection(particle1, particle2, box_size):
-    """
+def rdf_collection(particle1, particle2, box_size):                                      
+    """                                                                                    
     Method to calculate the radial distribution at a single time period
+    
     Uses the equation:
-    RDF_collection = sigma (delta |rij -r|)
-
+    rdf_collection = sigma (delta |rij -r|)
+    
     :param particle1: a Particle3D instance.
     :param particle2: another Particle3D instance.
-
     :return: the distance between two Particles
     """
     r12 = np.linalg.norm( image_closest_to_particle(particle1, particle2, box_size) )
 
     return r12
 
-def RDF_normalisation(RDF_collection, rho, num_step, bin_mids):
+def rdf_normalisation(rdf_collection, rho, num_collections, bin_mids):
     """
-    Method to normalise the radial distribution function
+    Method to normalise the radial distribution function. 
+    The bin width has been hardcoded to be 0.05 (reduced units).
+    
     Uses the Normalisation factor:
-    RDF_normal = (1/4pi*rho*dr*num_step*(r^2))*RDF_collection
-
+    rdf_normal = [1/(4pi*rho*dr*num_step*(r^2))]*RDF_collection
+    
     :param RDF_collection: an array of the number of items in each bin, unnormalised
     :param rho: the number density
-    :param num_step: the number of timesteps in the simulation
+    :param num_collections: the number of times RDF data was collected. This is hardcoded in the main().
     :param bin_mids: a list of the middle of each bin
-
-    :return: a list of the normalisation factor (to divide by) for
-    each bin in the simulation
+    :return: a list of the normalised RDF values, later to be plotted against the middle values of each bin.
     """
     norm_factor = []
+    dr = 0.05     
     for i in range (0, len(bin_mids)):
-        norm_factor.append(4*math.pi*rho*0.01*num_step*(bin_mids[i])**2)
-    return RDF_collection/np.asarray(norm_factor)
+        norm_factor.append(4*math.pi*rho*dr*num_collections*(bin_mids[i])**2)
+    return rdf_collection/np.asarray(norm_factor)
+
 
 # Begin main code
 def main():
@@ -219,7 +218,6 @@ def main():
     # create variable for the box size, using the same box size as in MDUtilities.py module
     box_size = (num_particles/rho)**(1./3.)
 
-
     # Create a list of Particle3D objects, with arbitrary positions and velocities.
     # Will use the arbitrary position (1, 0, 0) and velocity (1, 0, 0)
     # The list should have length equal to the user-defined number of particles, num_particles.
@@ -234,11 +232,10 @@ def main():
     set_initial_positions(rho, particles)
     set_initial_velocities(temp, particles)
 
-    # create list of initial positions for MSD function
+    # create list of initial positions for msd function
     initial_pos_list = []
     for j in range (num_particles):
         initial_pos_list.append(particles[j].position)
-
 
     # Print the number of particles and a header line to the trajectory file traj.xyz.
     # Print the initial positions of all the particles in the list 'particles' to the trajectory file.
@@ -262,12 +259,23 @@ def main():
     total_list = []
     time_list = []
 
+    # open a file to which the MSD values of the system will be written to. This will be of the format:
+    # <time> <MSD>
+    msd_file = open("msd.dat", "w")
     #initialise list of MSD over time
-    MSD_list = []
+    msd_list = []
 
-    #initialise list of RDF over time
-    RDF_list = []
-
+    # open a file to which the rdf values of the system will be written to. This will be of the format:
+    # <distance> <RDF value at this distance>
+    rdf_file = open("rdf.dat", "w")
+    # create a list for the calculated RDF values
+    rdf_list = []
+    #create list of bins to bin RDF values into. The value listed will be the mid-value of each bin.
+    # each bin will have a size of 0.05 in reduced units                                                    
+    bins = []  
+    for j in range (0, int(box_size*20)):    
+        bins.append(np.around((0.05*j), 2))   # make sure the numbers only have 2 decimal places
+    
     # Set time and energies to zero
     time = 0.
 
@@ -300,7 +308,18 @@ def main():
 
         # Re-define force values
         forces1 = forces1_new
+        
+        # write new positions to trajectory file, for every nth timestep (named print_int) using the modulo function
+        # At each nth timestep, write the energies to the file energy.dat, in format given above.
+        if (i+1)%print_int == 0:                                 # print to trajectory file if timestep is a multiple of print_int
+            output_file.write(str(num_particles) + "\n")
+            output_file.write("timestep = " + str(i+1) + "\n")
+            for j in range (0, num_particles):
+                output_file.write(str(particles[j]) + "\n")
 
+        else:
+            continue  # don't print to traj file if timestep is a multiple of print_int        
+        
         # add up kinetic and potential energy of the system:
         for j in range (0, num_particles):
             kin_energy += particles[j].kinetic_energy()
@@ -324,63 +343,68 @@ def main():
         total_list.append(total_energy)
         time_list.append(time)
 
-        #time integration element for MSD
+        #time integration element for MSD                                                   
+        #set total MSD to zero, then add the Squared Difference to sum and divide by N to find mean
+        totalMSD = 0                  
+        for j in range (0, num_particles):
+            totalMSD += msd(particles[j], initial_pos_list[j], box_size)/num_particles  
+        #append the total MSD for this timestep to the list of MSDs
+        msd_list.append(totalMSD)
+        
+        # write totalMSD to file, with the appropriate time in the format <time> <MSD>
+        msd_file.write("{0:f} {1:12.8f}\n".format(time, totalMSD))
 
-        totalMSD = 0                  #initialise the total MSD
-        for i in range (0, num_particles):
-            for j in range (0, num_particles):
-                if i != j:
-                    continue          #ensures the MSD between a particle and itself is not calculated
-                else:
-                    totalMSD += MSD(particles[i], initial_pos_list[j], box_size)/num_particles  #adds the Squared Difference to sum and divides by N to find mean
-
-        MSD_list.append(totalMSD)
-
-
-        #time integration element for RDF
+        # Begin data collection for plotting the RDF function
         """
-        so, the plan here is to collect the radial distance of each of the particles from each other, and then
-        round them to the nearest 0.01 (essentially making bins of 0.01 reduced units in length).  Then I made a list of all of the bin
-        boundaries (0.01, 0.02, 0.03... etc.) and counted how many items in the rounded list that there were that matched the
-        values of the bins specified in the "bins" list.  The number of items in each list was then recorded in the list count_for_bins
-        and this could be divided by the normalisation factor to give the y values for historgram.
+        The strategy here is to calculate the distances between all the pairs of particles (accounting for MIC).
+        These will be rounded to the nearest 0.05,(essentially placing each value into bins of 0.05 reduced units in length).  
+        This will be done at regular intervals and all values will be appended to the list rdf_list. 
+        
+        Later, (outside of the time-integration loop), the number of values in rdf_list falling into each bin will be counted,
+        and normalised using the average number of particles expected at this distance. 
+        This will then be plotted against the appropriate bin values.        
         """
-        for k in range (0, num_particles):
-            if k == 1:
-                continue          #ensures distance not calculated for a distance and itself
-            else:
-                RDF_list.append(RDF_collection(particles[k], particles[1], box_size))
-        RDF_rounded = np.around(RDF_list, 2)
-        count_for_bins = []
-        bins = []       #makes list of all bins used for RDF
-        for j in range (0, int(box_size*100)):
-            bins.append(0.01*j)
-        for i in range (0, len(bins)):
-            count_for_bins.append(np.count_nonzero(RDF_rounded == bins[i]))
-        count_array = np.asarray(count_for_bins)
-
-
-
-        # write new positions to trajectory file, for every nth timestep (named print_int) using the modulo function
-        # At each nth timestep, write the energies to the file energy.dat, in format given above.
-        if (i+1)%print_int == 0:                                 # print to trajectory file if timestep is a multiple of print_int
-            output_file.write(str(num_particles) + "\n")
-            output_file.write("timestep = " + str(i+1) + "\n")
+        # Only do this every 250th timestep to cut down on running time
+        # Note: If this is changed, also need to change num_collections when using the rdf_normalisation function.
+        if (i+1)%250 == 0:    # Do i+1 to avoid the zeroth timestep!
+            # Calculate the distances between each particle pair, making sure not to double count
+            # Then round to the nearest 0.05 and append this to rdf_list
             for j in range (0, num_particles):
-                output_file.write(str(particles[j]) + "\n")
-
-        else:
-            continue  # don't print to traj file if timestep is a multiple of print_int
-    #testing making a histogram for the RDF_list
-    norm_bins = RDF_normalisation(count_array, rho, numstep, bins)
-    #print(len(count_for_bins))
-
-
+                for k in range (0, num_particles):
+                    if j==k:        # avoid calculating distance between particle and itself
+                        continue
+                    elif j>k:          # prevent double counting pairs of particles
+                        continue
+                    else:
+                        rounded_number = round((rdf_collection(particles[j], particles[k], box_size))/0.05)*0.05
+                        rdf_list.append(np.around(rounded_number, 2))     # ensure that the numbers only have 2 decimal places!
+    # End of time integration loop
+    
+    # Continue with constructing the RDF:
+    # create a list which will hold the bin counts   
+    count_for_bins = []
+    # Count the number of values in rdf_list which fall into each bin. Append this to count_for_bins.
+    for j in range (0, len(bins)):
+        count_for_bins.append(np.count_nonzero(np.array(rdf_list) == bins[j]))   # Note: need to turn rdf_list into a numpy array so that we can use np.count_nonzero function
+    
+    #normalise these bins. 
+    # Set num_collections = 4 because program is hard-coded to collect only at 3 intervals (timesteps 249, 449 and 749, 999)
+    norm_bins = rdf_normalisation(np.array(count_for_bins), rho, 4, bins)
+    
+    # plot the radial distribution function
     pyplot.plot(bins, norm_bins)
     pyplot.title("Radial Distribution Function")
-    pyplot.ylabel("No. Particles")
-    pyplot.xlabel("r*")
+    pyplot.ylabel("RDF")
+    pyplot.xlabel("Distance in reduced units (r*)")
     pyplot.show()
+    
+    # Plot system MSD against time
+    pyplot.title('MSD vs time')
+    pyplot.xlabel('Time in reduced units (t*)')
+    pyplot.ylabel('MSD in reduced units (r*^2)')
+    line, = pyplot.plot(time_list, msd_list)
+    pyplot.show()
+    
     # Plot system energy to screen
     pyplot.title('Energy vs time')
     pyplot.xlabel('Time in reduced units (t*)')
@@ -397,17 +421,16 @@ def main():
     ax = pyplot.gca().add_artist(third_legend)
     pyplot.show()
 
+    # print RDF values to a file, with format <distance> <RDF value at this distance> (all in reduced units)
+    for i in range (len(bins)):
+        rdf_file.write("{0:f} {1:12.8f}\n".format(bins[i], norm_bins[i]))
+     
     # Post-simulation:
     # Close output file and energy.dat file
     energy_file.close()
+    msd_file.close()
+    rdf_file.close()
     output_file.close()
-
-    # Plot system MSD over time
-    pyplot.title('MSD vs time')
-    pyplot.xlabel('Time in reduced units (t*)')
-    pyplot.ylabel('MSD in reduced units (r*^2)')
-    line, = pyplot.plot(time_list, MSD_list)
-    pyplot.show()
 
 # Execute main method, but only when directly invoked
 if __name__ == "__main__":
